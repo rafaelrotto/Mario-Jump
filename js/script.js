91e5ff459c2ce06e71ee3ccbca47fd101d20ad65
Rafael <rafaelrotto@gmail.com>
2022-08-19 09:12:34 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+const clouds = document.querySelector('.clouds')
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() =>{
+        mario.classList.remove('jump');
+    } , 500)
+}
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudsPosition = clouds.offsetLeft;
+    
+    if(pipePosition <= 110 && pipePosition > 0 && marioPosition < 85){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './img/gameover.png';
+        mario.style.width = '145px';
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
+
+        clearInterval(loop);
+    }
+}, 10)
+
+document.addEventListener('keydown', jump);
